96c6c71fadf0a84102b26394ba166fba92547af8
Pierre Jacquin <Pierre.jacquin@mindmatcher.org>
2023-02-28 17:37:21 +0100

    Added Statistics, caching for performances

--- spotifyBRC.py
+++ spotifyBRC.py
@@ -9,8 +9,21 @@ st.set_page_config(
     page_title="Spotify Bar Race Chart",
     page_icon="spotifyIcon.ico",
 )
+
+@st.cache_data()
+def openZipFile()->pd.DataFrame:
+    df = pd.DataFrame()
+    with zipfile.ZipFile(st.session_state.file,"r") as z:
+        for file in z.namelist():
+            if file.startswith("MyData/endsong"):
+                json = pd.read_json(z.open(file))
+                df = pd.concat([df,json])
+    df = df[~df["master_metadata_track_name"].isna()].reset_index()
+    return df
+
+@st.cache_data()
 def process(df:pd.DataFrame)->pd.DataFrame:
-    serie = df["ts"].apply(lambda x : x.split("T")[0][:7])
+    serie = df["ts"].apply(lambda x : str(x).split("T")[0][:7])
     timeSerie = serie.apply(lambda x: datetime(int(x.split("-")[0]),int(x.split("-")[1]),day=1))
     df["ts"]=timeSerie
     df = df[["ts","master_metadata_track_name","master_metadata_album_artist_name"]]
@@ -21,6 +34,7 @@ def process(df:pd.DataFrame)->pd.DataFrame:
     df = df.sort_values("date")
     return df
 
+st.cache_data()
 def barRaceChart(df:pd.DataFrame,obj:str,bars:int,cmap:str):
     
     with st.spinner("Generating video ..."):
@@ -40,12 +54,36 @@ def barRaceChart(df:pd.DataFrame,obj:str,bars:int,cmap:str):
                                         n_bars=bars,
                                         cmap=cmap,
                                         filter_column_colors=True,
-                                        title="My Spotify Bar Race Chart") 
+                                        title="My Spotify Bar Race Chart")
         start = html_str.find('base64,')+len('base64,')
         end = html_str.find('">')
 
         video = base64.b64decode(html_str[start:end])
-        st.video(video)
+    return (video)
+
+def loadSidebar(df:pd.DataFrame):
+    with st.sidebar:
+        st.markdown("<h1 style='text-align: center; font-size:2.5em;'> Statistics </h1>",unsafe_allow_html=True)
+        st.markdown("---")
+        left,right = st.columns([1,1])
+        left.metric("Number of plays",len(df))
+        right.metric("Period (days)",(df["date"].max()-df["date"].min()).days)
+
+        left.metric("Differents artists",len(df["artists"].unique()))
+        right.metric("Differents songs",len(df["songs"].unique()))
+
+        st.markdown("---")
+        left,right = st.columns([1,1])
+
+        artist10 = df["artists"].value_counts()[:10].to_frame().rename(columns={"artists":"Plays"})
+        left.title("Top artists")
+        left.table(artist10)
+
+        songs10 = df["songs"].value_counts()[:10].to_frame().rename(columns={0:"Plays"})
+        right.title("Top songs")
+        right.table(songs10)
+    return None
+
 st.title("Upload your 'My_Spotify_Data.zip'")
 st.session_state.file = st.file_uploader("Upload your 'My_Spotify_Data.zip' file",type="zip",label_visibility="collapsed")
 
@@ -54,25 +92,24 @@ if st.session_state.file is None:
     st.image("tutorial.png")
     st.info("To download those, you must first request them on your Spotify account, under the privacy tab.  It may takes up to a week to be available")
 else:
-    df = pd.DataFrame()
-    with zipfile.ZipFile(st.session_state.file,"r") as z:
-        for file in z.namelist():
-            if file.startswith("MyData/endsong"):
-                json = pd.read_json(z.open(file))
-                df = pd.concat([df,json])
-    df = df[~df["master_metadata_track_name"].isna()].reset_index()
-    df = process(df)
+    data = openZipFile()
+    df = process(data)
+    loadSidebar(df)
     with st.form("Settings"):
         _,center,_ = st.columns([1,1,1])
         obj = center.radio("What do you want to track :",["artists","songs"],horizontal=True)
         bars = st.slider("Number of bars to display :",5,15,10)
-        cmap = st.select_slider("Color Palette :",['viridis', 'plasma', 'inferno', 'magma', 'cividis'],)
+        cmap = st.select_slider("Color Palette :",['spring', 'summer', 'autumn', 'winter'])
         
         timeframe_start,timeframe_end =  st.select_slider("What timeframe should I look at",df["date"],value=(df["date"].min(),df["date"].max()),format_func=lambda x: str(x)[:7].replace("-","/"))
         df = df[(df['date'] >= timeframe_start) & (df['date'] <= timeframe_end)]
         left,right = st.columns([1,1])
-        if st.form_submit_button("Load Graph"):
-            barRaceChart(df,obj,bars,cmap)
+        if st.form_submit_button("Load Video"):
+            st.session_state.video = barRaceChart(df,obj,bars,cmap)
+    if "video" in st.session_state:
+        st.video(st.session_state.video)
+        left,_,center,_,right = st.columns([1,1,1,1,1])
+        center.download_button("Download",st.session_state.video,file_name="SpotifyBRC.m4v")
     
 
 
