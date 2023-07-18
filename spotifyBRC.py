eb728f6f7d39717fc32036781c2dd0ceecaef368
Pierre Jacquin <Pierre.jacquin@mindmatcher.org>
2023-07-18 09:12:26 +0200

    fancyfied

--- spotifyBRC.py
+++ spotifyBRC.py
@@ -5,12 +5,17 @@ import bar_chart_race as bcr
 import base64
 import zipfile
 import ffmpeg
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.metric_cards import style_metric_cards
+from streamlit_extras.mandatory_date_range import date_range_picker
 st.set_page_config(
     page_title="Spotify Bar Race Chart",
     page_icon="spotifyIcon.ico",
 )
+    
 
-
+#Parse data from the spotify export
+@st.cache_data(ttl=600)
 def openZipFile()->pd.DataFrame:
     df = pd.DataFrame()
     with zipfile.ZipFile(st.session_state.file,"r") as z:
@@ -21,7 +26,8 @@ def openZipFile()->pd.DataFrame:
     df = df[~df["master_metadata_track_name"].isna()].reset_index()
     return df
 
-
+#Pre Process the data
+@st.cache_data(ttl=600)
 def process(df:pd.DataFrame)->pd.DataFrame:
     serie = df["ts"].apply(lambda x : str(x).split("T")[0][:7])
     timeSerie = serie.apply(lambda x: datetime(int(x.split("-")[0]),int(x.split("-")[1]),day=1))
@@ -34,8 +40,9 @@ def process(df:pd.DataFrame)->pd.DataFrame:
     df = df.sort_values("date")
     return df
 
-
-def barRaceChart(df:pd.DataFrame,obj:str,bars:int,cmap:str):
+#Run the bar race chart with adequate parameters
+st.cache_data()
+def barRaceChart(df:pd.DataFrame,obj:str,bars:int,cmap:str)->str:
     
     with st.spinner("Generating video ..."):
         top100 = df[obj].value_counts()[:100].index
@@ -61,10 +68,11 @@ def barRaceChart(df:pd.DataFrame,obj:str,bars:int,cmap:str):
         video = base64.b64decode(html_str[start:end])
     return (video)
 
+#Load basic stats on the sidebar
 def loadSidebar(df:pd.DataFrame):
     with st.sidebar:
-        st.markdown("<h1 style='text-align: center; font-size:2.5em;'> Statistics </h1>",unsafe_allow_html=True)
-        st.markdown("---")
+        colored_header("All-Time Statistics","general statistics about your account","green-70")
+
         left,right = st.columns([1,1])
         left.metric("Number of plays",len(df))
         right.metric("Period (days)",(df["date"].max()-df["date"].min()).days)
@@ -72,39 +80,54 @@ def loadSidebar(df:pd.DataFrame):
         left.metric("Differents artists",len(df["artists"].unique()))
         right.metric("Differents songs",len(df["songs"].unique()))
 
-        st.markdown("---")
+        style_metric_cards(border_left_color="#1DB954")
+
         left,right = st.columns([1,1])
-
-        artist10 = df["artists"].value_counts()[:10].to_frame().rename(columns={"artists":"Plays"})
-        left.title("Top artists")
-        left.table(artist10)
-
-        songs10 = df["songs"].value_counts()[:10].to_frame().rename(columns={0:"Plays"})
-        right.title("Top songs")
-        right.table(songs10)
+        with left:
+            colored_header("Top 10 artists","Your Top 10 most listened artists","green-70")
+            artist10 = df["artists"].value_counts()[:10].to_frame().rename(columns={"artists":"Plays"})
+            st.table(artist10)
+        with right:
+            colored_header("Top 10 songs","Your Top 10 most listened songs","green-70")
+            songs10 = df["songs"].value_counts()[:10].to_frame().rename(columns={0:"Plays"})
+            st.table(songs10)
     return None
 
-st.title("Upload your 'My_Spotify_Data.zip'")
+#Landing page
+if "file" not in st.session_state:
+    desc = "You can ask for this file on your Spotify privacy page"
+else:
+    desc = "Well done !"
+colored_header("Upload your 'My_Spotify_Data.zip'",desc,"green-70")
 st.session_state.file = st.file_uploader("Upload your 'My_Spotify_Data.zip' file",type="zip",label_visibility="collapsed")
-
 if st.session_state.file is None:
     st.warning("You want to upload your extended streaming history, not your account data")
     st.image("tutorial.png")
     st.info("To download those, you must first request them on your Spotify account, under the privacy tab.  It may takes up to a week to be available")
+
+#Main function, ask for parameters then run the bar race chart and display it
 else:
     data = openZipFile()
     df = process(data)
     loadSidebar(df)
+
     with st.form("Settings"):
+        colored_header("Video parameters","Select the timeframe and choose the appearance of your chart","green-70")
         _,center,_ = st.columns([1,1,1])
         obj = center.radio("What do you want to track :",["artists","songs"],horizontal=True)
-        bars = st.slider("Number of bars to display :",5,15,10)
-        cmap = st.select_slider("Color Palette :",['spring', 'summer', 'autumn', 'winter'])
-        
-        timeframe_start,timeframe_end =  st.select_slider("What timeframe should I look at",df["date"],value=(df["date"].min(),df["date"].max()),format_func=lambda x: str(x)[:7].replace("-","/"))
+        left,center,right = st.columns(3)
+        bars = left.number_input("Number of bars to display :",5,15,10)
+        cmap = center.selectbox("Color Palette :",['spring', 'summer', 'autumn', 'winter'])
+        with right:
+            timeframe_start,timeframe_end = date_range_picker("What timeframe should I look at",df["date"].min(),df["date"].max())
+        timeframe_start = datetime.combine(timeframe_start,datetime.min.time())
+        timeframe_end = datetime.combine(timeframe_end,datetime.max.time())
         df = df[(df['date'] >= timeframe_start) & (df['date'] <= timeframe_end)]
+        
+        left,right = st.columns([1,1])
         if st.form_submit_button("Load Video"):
             st.session_state.video = barRaceChart(df,obj,bars,cmap)
+            
     if "video" in st.session_state:
         st.video(st.session_state.video)
         left,_,center,_,right = st.columns([1,1,1,1,1])
